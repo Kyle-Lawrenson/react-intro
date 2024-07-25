b0ffb471900c07e0538f1913acae57b6ecf4eddb
Kyle Lawrenson <kylelawrenson99@gmail.com>
2024-07-25 10:02:30 +0100

    started to add pet info

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@
 import './App.css'
 import Job from './Job'
 import Person from './Person'
-// import Pet from './Pet'
-
+import Pet from './Person'
 const App = () => {
 
   return (
@@ -11,7 +10,7 @@ const App = () => {
        <h2>My First React App</h2> 
        <Person name="Superman" age="40" />
        <Job jobname="Software Developer"/>
-       <Person name="Batman" age="48" />
+       <Person name="Batman" age="48" Pet/>
        <Job jobname="Millionnair"/>
        <Person name="Thor" age="290" />
        <Job jobname="Carpenter"/>
--- src/Person.jsx
+++ src/Person.jsx
@@ -3,7 +3,17 @@ const Person = (props) => {
 	return (
 	<div>
 	    <h2>My name is {props.name} and my age is {props.age}.</h2>
-        {/* <Pet name="Dave The Magical Cheese Wizard" /> */}
+        
+		
+	</div>
+	)
+}
+const Pet = (petsName, type) => {
+
+	return (
+	<div>
+	    <h2>My {type} is called {petsName}.</h2>
+		<Pet petsName={'Dave The Magical Cheese Wizard'} type={'cat'} />
 	</div>
 	)
 }
